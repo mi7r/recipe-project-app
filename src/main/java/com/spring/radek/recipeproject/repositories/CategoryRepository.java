94a16b630bfe0e9c777a8d881da3d40995d19a3f
mi7r <michalski.rados@gmail.com>
2018-12-12 15:35:09 +0100

    data.sql added. Repository modified. Index Controller modified.

--- src/main/java/com/spring/radek/recipeproject/repositories/CategoryRepository.java
+++ src/main/java/com/spring/radek/recipeproject/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.spring.radek.recipeproject.repositories;
 import com.spring.radek.recipeproject.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
