42cdb76b1455061c2678b6fad9ac38bff27161ee
mi7r <michalski.rados@gmail.com>
2019-01-03 15:49:53 +0100

    update to ProjectLombok

--- src/main/java/com/spring/radek/recipeproject/services/RecipeService.java
+++ src/main/java/com/spring/radek/recipeproject/services/RecipeService.java
@@ -3,6 +3,8 @@ package com.spring.radek.recipeproject.services;
 import com.spring.radek.recipeproject.domain.Recipe;
 import java.util.Set;
 
+
+
 public interface RecipeService {
 
     Set<Recipe> getRecipes();
