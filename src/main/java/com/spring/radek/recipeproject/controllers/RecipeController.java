bc856dd07637d090487282772862912b3118a50c
Radek <michalski.rados@gmail.com>
2019-03-20 12:04:46 +0100

    recipeform.html corrected. Recipe Controller ready.

--- src/main/java/com/spring/radek/recipeproject/controllers/RecipeController.java
+++ src/main/java/com/spring/radek/recipeproject/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.spring.radek.recipeproject.controllers;
 
+import com.spring.radek.recipeproject.commands.RecipeCommand;
 import com.spring.radek.recipeproject.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,18 +25,18 @@ public class RecipeController {
         return "recipe/show";
     }
 
-//    @RequestMapping("/recipe/new")
-//    public String newRecipe(Model model) {
-//        model.addAttribute("recipe", new RecipeCommand());
-//
-//        return "recipe/recipeform";
-//    }
-//
-//    @PostMapping
-//    @RequestMapping("recipe")
-//    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
-//        RecipeCommand saveRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
-//
-//        return "redirect:/recipe/show" + saveRecipeCommand.getId();
-//    }
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand saveRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + saveRecipeCommand.getId();
+    }
 }
