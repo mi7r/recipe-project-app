8c74838862de60b62adef478ce008a72c2d36c34
Radek <michalski.rados@gmail.com>
2019-04-09 16:51:56 +0200

    Displaying Validation Errors with thymeleaf.

--- src/test/java/com/spring/radek/recipeproject/controllers/RecipeControllerTest.java
+++ src/test/java/com/spring/radek/recipeproject/controllers/RecipeControllerTest.java
@@ -77,10 +77,26 @@ public class RecipeControllerTest {
         mockMvc.perform(post("/recipe")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
-                .param("description", "some description"))
+                .param("description", "some description")
+                .param("directions", "some directions"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
 
+    }
+
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(recipeCommand);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", ""))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(view().name("recipe/recipeform"));
     }
 
     @Test
